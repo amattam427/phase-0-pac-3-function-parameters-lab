36883194345834ca89df4b14900d41b4f3b21bf9
Ashley Mattam <amattam427@gmail.com>
2021-11-13 03:23:11 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name = "Ashley"){
+    return `Hi, my name is ${name}.`
+}
+function introductionWithLanguage(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
